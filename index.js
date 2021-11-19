d1a2242db461679705d1e7589b96933be7ba8e9c
andrian firmansyah <andrian@cc-co.co>
2021-11-19 11:07:53 +0700

    Updates

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const material = new THREE.MeshBasicMaterial({
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
+const geometry2 = new THREE.BoxGeometry(2,2,2)
+const material2 = new THREE.MeshBasicMaterial({
+    color:0xffffff,
+    wireframe: true
+})
+const mesh2 = new THREE.Mesh(geometry2,material2)
+mesh2.position.x = 3;
+scene.add(mesh2)
+
+const geometry3 = new THREE.TorusKnotGeometry( 1, 0.2, 76, 7, 3, 4 );
+const material3 = new THREE.MeshBasicMaterial( { color: 0xffff00, wireframe:true } );
+const torusKnot = new THREE.Mesh( geometry3, material3 );
+torusKnot.position.x = -3.5
+scene.add( torusKnot );
 
 
 //Boilerplate code
@@ -67,6 +81,12 @@ const tick = () =>
     // Update objects
     mesh.rotation.y = .8 * elapsedTime
     mesh.rotation.z = .8 * elapsedTime
+
+    mesh2.rotation.y = .2 * elapsedTime
+    mesh2.rotation.z = .2 * elapsedTime
+
+    torusKnot.rotation.y = .1 * elapsedTime
+    torusKnot.rotation.z = .1 * elapsedTime
 
     // Update Orbital Controls
     // controls.update()
